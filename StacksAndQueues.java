3c5d069c2cfdad0bfe5152c094b720711e3f6b93
Ana <ad12461@my.bristol.ac.uk>
2016-01-30 02:26:30 +0000

    .

---
+++ StacksAndQueues.java
@@ -0,0 +1,24 @@
+class Stack {
+    
+}
+
+public class StacksAndQueues {
+    /**
+     * 3.1
+     * Threee in One: Describe how you could use a single array to implement three stacks.
+     */
+
+    /**
+     * 3.2
+     * Stack Min: How would you design a stack which, in addition to push and pop, has a function
+     * min which returns the minimum elelment? Push, pop and min should all operate in O(1) time.
+     */
+    public static int stackMin(Stack stack) {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
