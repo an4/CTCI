c4c5edbe81740a31fb3bb4cbd2c0c3c8c0b29e3a
Ana <ad12461@my.bristol.ac.uk>
2016-02-01 12:51:55 +0000

    .

--- StacksAndQueues.java
+++ StacksAndQueues.java
@@ -1,5 +1,40 @@
 class Stack {
-    
+    Node top;
+
+    public Node push(Integer data) {
+        return top.prepend(data);
+    }
+
+    public Integer peek() {
+        return top.data;
+    }
+
+    public Node pop() {
+        return top.next;
+    }
+}
+
+class Queue {
+    Node first, last;
+
+    void enqueue(Integer data) {
+        if(first == null) {
+            last = new Node(data);
+            first = last;
+        } else {
+            last.next = new Node(item);
+            last = last.next;
+        }
+    }
+
+    Integer dequeue() {
+        if(first != null) {
+            Integer d = first.data;
+            first = first.next;
+            return d;
+        }
+        return null;
+    }
 }
 
 public class StacksAndQueues {
@@ -13,9 +48,9 @@ public class StacksAndQueues {
      * Stack Min: How would you design a stack which, in addition to push and pop, has a function
      * min which returns the minimum elelment? Push, pop and min should all operate in O(1) time.
      */
-    public static int stackMin(Stack stack) {
-
-    }
+    // public static int stackMin(Stack stack) {
+    //
+    // }
 
 
     public static void main(String[] args) {
