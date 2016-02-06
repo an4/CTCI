530434602f223de09c8041c62b3febf6e758b0d9
an4 <ad12461@my.bristol.ac.uk>
2016-02-06 00:49:52 +0000

    .

--- RecursionAndDynamicProgramming.java
+++ RecursionAndDynamicProgramming.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class RecursionAndDynamicProgramming {
     /**
@@ -94,9 +95,10 @@ public class RecursionAndDynamicProgramming {
      * 2^n subsets
      */
     public static void powerSet(ArrayList<Integer> set) {
-        ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
+        HashSet<ArrayList<Integer>> output = new HashSet<ArrayList<Integer>>();
+        output.add(new ArrayList<Integer>());
         for(Integer a : set) {
-            ArrayList<ArrayList<Integer>> new_subsets = new ArrayList<ArrayList<Integer>>();
+            HashSet<ArrayList<Integer>> new_subsets = new HashSet<ArrayList<Integer>>();
             ArrayList<Integer> el_set = new ArrayList<Integer>(a);
             new_subsets.add(el_set);
             for(ArrayList<Integer> subset: output) {
@@ -117,6 +119,128 @@ public class RecursionAndDynamicProgramming {
         }
     }
 
+    /**
+     * 8.5
+     * Recursive Multiply: Write a recursive function to multiply two numbers without using the *
+     * operator. You can use addition, substraction, bit shifting, but you should minimize the
+     * number of thse operations.
+     */
+    public static int minProduct(int min, int max) {
+        if(min == 0) {
+            return 0;
+        }
+        if(min == 1) {
+            return max;
+        }
+        int s = min >> 1;
+        int half = minProduct(s, max);
+
+        if(min % 2 == 0) {
+            return half + half;
+        }
+        return half + half + max;
+    }
+
+    public static int recursiveMultiply(int a, int b) {
+        int min = a <= b ? a : b;
+        int max = a > b ? a : b;
+        return minProduct(min, max);
+    }
+
+    /**
+     * 8.6
+     * Towers of Hanoi: In the classic problem of the Towers of Hanoi, you have 3 towers and N
+     * disks of different sizes which can slide onto any tower. The puzzle starts with disks sorted
+     * in ascending order of size from top to bottom. (i.e., each disk sits on top of an even
+     * larger one). You have the following constraints:
+     * (1) Only one disk can be moved at a time.
+     * (2) A disk is slid off the top of one tower onto the next tower.
+     * (3) A disk can only be places on top of a larger disk.
+     * Write a progrm to move the disks from the first tower to the last using stacks.
+     */
+
+    /**
+     * 8.7
+     * Permutations without Dups: Write a method to compute all permutations of a string of unique
+     * characters.
+     */
+    public static void permutationsWithoutDups(String str) {
+        ArrayList<String> perm = new ArrayList<String>();
+        perm.add(str.substring(0,1));
+        for(int i=1; i<str.length(); i++) {
+            ArrayList<String> output = new ArrayList<String>();
+            for(String p: perm) {
+                for(int j=0; j<=p.length(); j++) {
+                    output.add(p.substring(0,j) + str.substring(i,i+1) + p.substring(j));
+                }
+            }
+            perm = output;
+        }
+
+        for(String s: perm) {
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * 8.8
+     * Permutations with Dups: Write a method to compute all permutations of a string whose
+     * characters are not necessarily unuque. The list of permutations should not have duplicates.
+     */
+    public static void permutationsWithDups(String str) {
+        HashSet<String> perm = new HashSet<String>();
+        perm.add(str.substring(0,1));
+        for(int i=1; i<str.length(); i++) {
+            HashSet<String> output = new HashSet<String>();
+            for(String p: perm) {
+                for(int j=0; j<=p.length(); j++) {
+                    output.add(p.substring(0,j) + str.substring(i,i+1) + p.substring(j));
+                }
+            }
+            perm = output;
+        }
+
+        for(String s: perm) {
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * 8.9
+     * Parens: Implement an algorithm to print all valid (e.g, properly opened and closed)
+     * combinations of n pairs of parantheses.
+     * EXAMPLE
+     * Input: 3
+     * Output: ((())), (()()), (())(), ()(()), ()()()
+     */
+    public static void parens_helper(String s, int open, int closed) {
+        if(open == 0 && closed == 0) {
+            System.out.println(s);
+            return;
+        }
+        if(open > 0) {
+            parens_helper(s+"(", open-1, closed);
+        }
+        if(open<closed) {
+            parens_helper(s+")", open, closed-1);
+        }
+    }
+
+    public static void parens(int n) {
+        parens_helper("", n, n);
+    }
+
+    /**
+     * 8.10
+     * Paint Fill: Implement the "paint fill" function that one might see on many image editing
+     * programs. That is, given a screen (represented by a two-dimensional array of colors), a
+     * point, and a new color, fill in the surrounding area until the color chnages from the
+     * original color.
+     */
+    public static void paintFill(int[][] screen, int x, int y, int color) {
+        
+    }
+
     public static void main(String[] args) {
         // System.out.println(tripleStep(4));
         int[] arr = {-1,0,2,6,7,8,9};
@@ -127,5 +251,11 @@ public class RecursionAndDynamicProgramming {
         test.add(2);
         test.add(3);
         powerSet(test);
+
+        // permutationsWithDups("aabc");
+
+        parens(3);
+
+
     }
 }
