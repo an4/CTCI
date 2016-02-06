05c3580926a2db60053f9c6762d7a3741db62e81
an4 <ad12461@my.bristol.ac.uk>
2016-02-06 21:38:39 +0000

    .

--- SortingAndSearching.java
+++ SortingAndSearching.java
@@ -2,6 +2,26 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+class BitSet {
+    int[] bitset;
+
+    public BitSet(int size) {
+        bitset = new int[(size>>5) + 1]; // divide by 32
+    }
+
+    boolean get(int pos) {
+        int wordNumber = (pos>>5); // divide by 32
+        int bitNumber = (pos& 0x1F); // mod 32
+        return (bitset[wordNumber] & (1 << bitNumber)) != 0;
+    }
+
+    void set(int pos) {
+        int wordNumber = (pos>>5); // divide by 32
+        int bitNumber = (pos & 0x1F); // mod 32
+        bitset[wordNumber] |= 1 << bitNumber;
+    }
+}
+
 public class SortingAndSearching{
     /**
      * 10.1
@@ -169,9 +189,124 @@ public class SortingAndSearching{
      * Input: ball, {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
      * Output: 4
      */
-    public static int sparseSearch(String[] arr) {
-        
+    public static int sparseSearch(String[] arr, String item) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end) {
+            int middle = (start+end)/2;
+            if(arr[middle].equals("")) {
+                int left = middle-1;
+                int right = middle+1;
+                while(arr[left].equals("") && arr[right].equals("") && left>=0 && right<arr.length) {
+                    left--;
+                    right++;
+                }
+                if(arr[left].equals("")) {
+                    middle = right;
+                } else {
+                    middle = left;
+                }
+            }
+            if(arr[middle].equals(item)) {
+                return middle;
+            }
+            if(arr[middle].compareTo(item) < 0) {
+                start = middle+1;
+            } else {
+                end = middle-1;
+            }
+        }
+        return -1;
     }
+
+    /**
+     * 10.6
+     * Sort Big File: Imagine you have a 20 GB file with one string per line. Explain how you
+     * would sort the file.
+     */
+    // External Sort
+
+    /**
+     * 10.7
+     * Missing Int: Given an input file with four billion non-negative integers, provide an
+     * algorithm to generate an integer that is not contained in the file. Assume you have 1 GB
+     * of memory available for the task.
+     * FOLLOW UP
+     * What if you have only 10 MB of memmory? Assume that all the values are distinct and we now
+     * have no more than one billion non-negative integers.
+     */
+    // public static int missingInt(String file) {
+    //     // 2^31 non-negative integers
+    // }
+
+    /**
+     * 10.8
+     * Find Dulicates: You have an array with all the numbers from 1 to N, where N is at most
+     * 32,000. The array may have duplicate entries and you do not know what N is. With only 4
+     * kylobytes of memory available, how would you print all duplicate elements in the array.
+     */
+    public static void findDuplicates(int[] array) {
+        BitSet bs = new BitSet(32000);
+
+        for(int i=0; i<array.length; i++) {
+            int num = array[i];
+            int num0 = num - 1; // bitset starts at 0, numbers start at 1
+            if(bs.get(num0)) {
+                System.out.println(num);
+            } else {
+                bs.set(num0);
+            }
+        }
+    }
+
+    /**
+     * 10.9
+     * Sorted Matrix Search: Given MxN matrix in which each row and each column is sorted in
+     * ascending order, write a method to find an element.
+     */
+    // public static void sortedMatrixSearch(int[][] matrix, int el) {
+    //
+    // }
+
+    /**
+     * 10.10
+     * Rank from Stream: Imagine you are reading in a stream of integers. Periodically, you wish
+     * to be able to look up the rank of a number x (the number of values less than or equal to x).
+     * Implement the data structure and algorithms to support these operations. That is,
+     * implement the method track (int x), which is called when each number is generated, and the
+     * method getRankOfNumber (int x), which returns the number of values less than or equal to x
+     * (not including x itself).
+     * EXAMPLE
+     * Stream (in order of appearance) 5, 1, 4, 4, 5, 9, 7, 13, 3
+     * getRankOfNumber(1) = 0;
+     * getRankOfNumber(3) = 1;
+     * getRankOfNumber(4) = 3;
+     */
+    // public static void track(int x) {
+    //
+    // }
+    //
+    // public static int getRankOfNumber(int x) {
+    //     return 0;
+    // }
+    //
+    // public static void rankFromStrem() {
+    //
+    // }
+
+    /**
+     * 10.11
+     * PeaksAndValleys: In an array of integers, a "peak" is an element which is greater than or
+     * or equal to the adjacent integers and a "valley" is an element which is less than or equal
+     * to the adjacent integers. For example, in the array {5,8,2,3,4,6}, {8,6} are peaks and
+     * {5,2} are valleys. Given an array of integers, sort the array into an alternating
+     * sequence of peaks and valleys.
+     * EXAMPLE
+     * Input: {5,3,1,2,3}
+     * Output: {5,1,3,2,3}
+     */
+    
+
 
 
     public static void main(String[] args) {
@@ -185,5 +320,8 @@ public class SortingAndSearching{
         // int[] arr = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         // System.out.println(sortedSearchNoSize(arr, 5));
         // System.out.println(sortedSearchNoSize(arr, 19));
+
+        // String[] arr =  {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
+        // System.out.println(sparseSearch(arr, "ball"));
     }
 }
